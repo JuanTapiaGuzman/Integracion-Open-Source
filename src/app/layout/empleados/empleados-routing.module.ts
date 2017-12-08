055c9acf4c4353eae47fa39267d2507069ca0397
Juan Tapia <juanatg96@gmail.com>
2017-12-08 07:16:34 -0500

    Fix on empleados

--- src/app/layout/empleados/empleados-routing.module.ts
+++ src/app/layout/empleados/empleados-routing.module.ts
@@ -1,32 +1,35 @@
-import { EmpleadosComponent } from './empleados.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmpleadosComponent } from './empleados.component';
 import { AddEmpleadoComponent } from 'app/layout/empleados/add-empleado/add-empleado.component';
 import { ListEmpleadoComponent } from 'app/layout/empleados/list-empleado/list-empleado.component';
 import { DetailEmpleadoComponent } from 'app/layout/empleados/list-empleado/detail-empleado/detail-empleado.component';
 import { EditEmpleadoComponent } from 'app/layout/empleados/list-empleado/edit-empleado/edit-empleado.component';
 
 const routes: Routes = [
-  { path: '', component: EmpleadosComponent,
-  children: [
-  { 
-    path: 'add', component: AddEmpleadoComponent
-  },
-  {
-    path: 'list', component: ListEmpleadoComponent,
+  { path: '', 
+    component: EmpleadosComponent,
     children: [
+      { 
+        path: 'add', 
+        component: AddEmpleadoComponent
+      },
       {
-        path: 'view/:empleado-id',
-          component: DetailEmpleadoComponent
-    },
-       {
-        path: 'edit/:empleado-id',
-          component: EditEmpleadoComponent
-    }
-  ]
+        path: 'list', 
+        component: ListEmpleadoComponent,
+        children: [
+          {
+            path: 'view/:empleado-id',
+            component: DetailEmpleadoComponent
+          },
+          {
+            path: 'edit/:empleado-id',
+            component: EditEmpleadoComponent
+          }
+        ]
+      }
+    ] 
   }
-] 
-}
 ];
 
 @NgModule({
